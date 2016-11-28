5a350e1e0d1297616fdc52a5965daad0e1e2d610
maegz <max.sundberg@hotmail.com>
2016-11-28 17:41:18 +0100

    Removing outcommented code, removing button in memory

--- js/memory.js
+++ js/memory.js
@@ -78,14 +78,14 @@ function showImage(li, value) {
                 imageIDs = [];
 
                 if (clearedImages == imagesArray.length) {
-                    $("#content2").fadeOut("slow");
-                    $("#content2").html("<h1 style='text-align: center;'>You did it!</h1>" +
-                                        "<button type='button' onclick(restartGame())>Play again?</button>").fadeIn("slow");
-                    wonGames += 1;
-                    averagePerGame = triesTotal / wonGames;
                     function restartGame() {
                         $("#content2").fadeOut(1500, function() { memoryGame(); });
                     }
+                    wonGames += 1;
+                    averagePerGame = triesTotal / wonGames;
+                    $("#content2").fadeIn("slow");
+                    $("#content2").html("<h1 style='text-align: center;'>You did it!</h1>");
+                    restartGame();
                 }
 
             } else {
@@ -106,116 +106,3 @@ function showImage(li, value) {
         }
     }
 }
-
-    // console.log(li + " " + value);
-    // $(li).children("img").fadeIn("fast");
-
-    // function showImage(image, number) {
-    //     if (image.src == "" && imageValues.length < 2) {
-    //         image.src = number; // Image = imagesArray[i].
-    //         if (imageValues.length == 0) {
-    //             imageValues.push(number); // Push imagesArray[i] into imageValues.
-    //             imageIDs.push(image.id); // Push images id into imageIDs.
-    //         } else if (imageValues.length == 1) {
-    //             imageValues.push(number); // Push imagesArray[i] into imageValues.
-    //             imageIDs.push(image.id); // Push images id into imageIDs.
-    //             if (imageValues[0] == imageValues[1]) {
-    //                 clearedImages += 2;
-    //                 imageValues = [];
-    //                 imageIDs = [];
-
-    //                 if (clearedImages == imagesArray.length) {
-    //                     console.log("Woopwoop!");
-    //                     // GVEWHUIGOWENGOIBWNEOBINBIO
-    //                 } else {
-    //                     function hideUnmatchedImages() {
-    //                         $(imageIDs[0]).children("img").fadeOut("slow");
-    //                         $(imageIDs[1]).children("img").fadeOut("slow");
-    //                         // document.getElementById(memory_tile_ids[0]
-    //                         imageValues = [];
-    //                         imageIDs = [];
-    //                     }
-    //                     setTimeout(hideUnmatchedImages, 1000);
-    //                 }
-    //             }
-    //         }
-    //     }
-    // }
-
-
-
-
-
-    // $("ol li").click(function() {
-    //     if ((count < 2) && ($(this).children("img").hasClass("face-up")) === false) {
-    //         // Guessing count + 1, memoryLi-child not hidden and gets face-up class added.
-    //         count++;
-    //         $(this).children("img").fadeIn("fast");
-    //         $(this).children("img").addClass("face-up");
-    //
-    //         // Guess #1
-    //         if (count === 1 ) {
-    //             guess1 = $(this).children("img").attr("src"); // get memoryLi-child #1 image name.
-    //         }
-    //         // Guess #2
-    //         else {
-    //             guess2 = $(this).children("img").attr("src"); // get memoryLi-childs #2 image name.
-    //
-    //             // Second guess, check for a match. If that is the scenario, add class match to memoryLi-child.
-    //             if (guess1 === guess2) {
-    //                 console.log("Correct!");
-    //                 $("ol li").children("img[src='" + guess2 + "']").addClass("match");
-    //             }
-    //             // If not a match, image gets hidden and class face-up is removed.
-    //             else {
-    //                 console.log("Miss!");
-    //                 setTimeout(function() {
-    //                     $("ol li").children("img").not(".match").fadeOut("slow");
-    //                     $("img").not(".match").removeClass("face-up");
-    //                 }, 1000);
-    //             }
-    //             // Reset counter.
-    //             count = 0;
-    //         }
-    //     }
-    // });
-
-
-//
-// $("ol li").click(function() {
-//     if ((count < 2) && ($(this).children("img").hasClass("face-up")) === false) {
-//         // Guessing count + 1, memoryLi-child not hidden and gets face-up class added.
-//         count++;
-//         $(this).children("img").fadeIn("fast");
-//         $(this).children("img").addClass("face-up");
-//
-//         // Guess #1
-//         if (count === 1 ) {
-//             guess1 = $(this).children("img").attr("src"); // get memoryLi-child #1 image name.
-//         }
-//         // Guess #2
-//         else {
-//             guess2 = $(this).children("img").attr("src"); // get memoryLi-childs #2 image name.
-//
-//             // Second guess, check for a match. If that is the scenario, add class match to memoryLi-child.
-//             if (guess1 === guess2) {
-//                 console.log("Correct!");
-//                 $("ol li").children("img[src='" + guess2 + "']").addClass("match");
-//             }
-//             // If not a match, image gets hidden and class face-up is removed.
-//             else {
-//                 console.log("Miss!");
-//                 setTimeout(function() {
-//                     $("ol li").children("img").not(".match").fadeOut("slow");
-//                     $("img").not(".match").removeClass("face-up");
-//                 }, 1000);
-//             }
-//             // Reset counter.
-//             count = 0;
-//         }
-//     }
-// });
-
-
-    //
-    //

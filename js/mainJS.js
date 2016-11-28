903f84ec37359adedc556c342e5f5b1df799549b
maegz <max.sundberg@hotmail.com>
2016-11-28 17:24:14 +0100

    Most work done. Missing function of button in memory.js

--- js/mainJS.js
+++ js/mainJS.js
@@ -29,7 +29,7 @@ function createTabContent(content) {
         case "content1":
             // Just a description of the assignment, with grade-chart.
             content1.innerHTML =
-                "<h2>About the Project</h2>" +
+                "<h2>About the Project:</h2>" +
                 "<p>The projects purpose is to show how much we've accually understood " +
                     "and learned during the past few weeks.<br>" +
                     "It will contain some Bootstrap and jQuery-goodies and " +
@@ -37,7 +37,35 @@ function createTabContent(content) {
                     "Also a CSS-switcher, to show my desires and love of style. " +
                     "Last but not the least I'll link all my other projects " +
                     "and pages." +
-                "</p>";
+                "</p><br>" +
+                "<h2>Gradings:<h2>" +
+                "<h3>Accepted (G):</h3>" +
+                "<p>- Make project and code all by myself.</p>" +
+                "<p>- Be able to tell about the code and why I did it like so.</p>" +
+                "<p>- Have at least one HTML-, CSS- and JS-file.</p>" +
+                "<p>- Use Bootstrap for some part of the design.</p>" +
+                "<p>- Have at least one Media Query.</p>" +
+                "<p>- Add design with my own CSS extended over the Bootstrap-CSS.</p>" +
+                "<p>- Use jQuery for two different parts. But also at least one DOM-manipultaion with only JS.</p>" +
+                "<p>- Make a styled form.</p>" +
+                "<p>- Use HTML5-tags where it's semanticly correct to do so.</p>" +
+                "<p>- Have a proper, neat readme-file on GitHub, explaining that it's a " +
+                "schoolproject, what course it is, wich school, technology usage, " +
+                "maker of the project and a link to my LinkedIn.</p>" +
+                "<p>- Have a page that looks good on at least two screen-sizes: mobile and computer.</p>" +
+                "<p>- Have a working menu that fits for the screen-size.</p>" +
+                "<h3>Very accepted (VG):</h3>" +
+                "<p>- Make everything in Accepted (G).</p>" +
+                "<p>- Make the page well thought through with the design. "+
+                "Giving a whole where design and functionality works together.</p>" +
+                "<p>- Manipulate CSS, the DOM and make some effect/animation using jQuery.</p>" +
+                "<p>- Have some form of JS-functionality. jQuery is allowed.</p>" +
+                "<p>- Complete project before deadline.</p>" +
+                "<p>- Have a tidy readme on GitHub, giving a short explanation about the workprocess " +
+                "and tell about what I think I did well, and what I could work on to make better.</p>" +
+                "<p>- Have a page that looks good on at least three screen-sizes.</p>" +
+                "<p>- Make a opposition on another students project and tell about three things that are well done " +
+                "and three things that could be made better.</p><br><br>";
             break;
         case "content2":
             // Memory game. More info in the memory.js-file.
@@ -47,7 +75,8 @@ function createTabContent(content) {
             // Switch stylesheet through a select element's option elements.
             // When selected, the swapStyleSheet()-function is activated.
             content3.innerHTML =
-                "<h2>Switch the Layout</h2>" +
+                "<h2>Switch the Layout:</h2>" +
+                "<p>Use the dropdown menu to change stylesheet.</p>" +
                 "<select id='selectSheet' onChange='swapStyleSheet()'>" +
                     "<option disabled='disabled' selected='selected'>Choose here:</option>" +
                     "<option value='css/master.css'>Default</option>" +
@@ -56,14 +85,38 @@ function createTabContent(content) {
                 "</select>";
             break;
         case "content4":
+            // Let user see more projects on GitHub.
             content4.innerHTML =
-                "<h2>Other Projects</h2>" +
-                "<p>!!!!!!!!!GITHUB-LINKS HERE!!!!!!!</p>";
+                "<h2>Other Projects:</h2>" +
+                "<ul>" +
+                    "<li><a href='https://github.com/maegz/Python-project'>Text-based escape game in Python.</a></li>" +
+                    "<li><a href='https://github.com/maegz/DPYett'>First 'digital presentation' in this course.</a></li>" +
+                    "<li><a href='https://github.com/maegz'>Just my GitHub-page..</a></li>" +
+                "</ul>";
+                content4.style.textAlign = "center"; // Get everything in content4 centerd.
+                var links = content4.children;
+                for (var i = 1; i < links.length; i++) {
+                    links[i].style.listStyleType = "none"; // Remove list-item marker.
+                    links[i].style.paddingRight = "35px"; // Pad text to right, so it looks more centered.
+                }
             break;
         case "content5":
             content5.innerHTML =
-                "<h2>About Me</h2>" +
-                "<p>!!!!!!ME HERE!!!!!</p>";
+                "<h2>About Me:</h2>" +
+                "<p>Welcome to the project site! I (the creator) am Max(imilan) Sundberg.</p>" +
+                "<p>I live in Uppsala, Sweden, and has been since I was born. Except when I went" +
+                "to Oslo, Norway to get some work experience and a little bit of easy cash.</p>" +
+                "<p>I'm a certified cook and have always been interested in cooking. It probalby comes " +
+                "from my father, since he's been in the kitchen, doing 12 hour meals as long as I can remember. " +
+                "Cooking is a passion, and I found out that working with it was ungrateful and sucked out all the " +
+                "inspiration. So I've descided to keep on cooking, but only for friends and family.</p>" +
+                "<p>Computers have always been interesting for me, but the programming part kind of spinned forward " +
+                "recently. Probably because of plenty of friends that work with it.</p>" +
+                "<p>Although my biggest passion is music! I've been playing drums since I was 11, and it always " +
+                "makes me smile!</p>" +
+                "<p>But when I don't work, cook or play the drums I prefer to travel. " +
+                "And when I say travel I mean it. Into the culture and explore, no darn coconut-drinks at the beach!</p><br>" +
+                "<p>Well.. that's a little something-something about me. Cheers!</p>";
             break;
 
         default: content.innerHTML = "";
--- js/memory.js
+++ js/memory.js
@@ -1,19 +1,3 @@
-
-
-
-/*
-                Before uploading:
-                - add score-board
-                - fill in functions for score-board
-                    - triesThisGame,
-                    - triesTotal,
-                    - wonGames,
-                    - median of tries/won games
-                    - each trie should be a pair.
-*/
-
-
-
 // Snowflake memory game!
 var imagesArray = [],
     imageValues = [],
@@ -21,6 +5,7 @@ var imagesArray = [],
     clearedImages = 0,
     triesThisGame = 0,
     triesTotal = 0,
+    averagePerGame = 0,
     wonGames = 0;
 
 // Get images and push them in an array twice.
@@ -41,34 +26,46 @@ function shuffle(array) {
 
 function memoryGame() {
     clearedImages = 0;
+    triesThisGame = 0;
     output = "";
     shuffle(imagesArray);
 
     // Output headline. Output images inside an ordered list, then hide them.
-    var output = "<h2 style='text-align: center;'>Let's play some memory!</h2><br><ol>";
+    var output = "<h2>Let's play some memory!</h2><br><ol>";
 
     for (var i = 0; i < imagesArray.length; i++) {
         // When clicking, function showImage(this, "imagesArray[i]") will start.
         output += "<li id='imageNr" + i + "' onclick='showImage(this, \"" + imagesArray[i] + "\")'></li>";
     }
+    // Close the list.
     output += "</ol>";
+    // Add a score-board with tries, won games and average tries/game.
+    output += "<h2>Score board:</h2><br><div id='scoreBoard'><p id='triesThisGame'>Tries this game: 0</p>" +
+    "<p>Won games: " + wonGames + "</p>" +
+    "<p>Average tries per game: " + averagePerGame + "</p></div>";
 
     // Put the output in content2.
     document.getElementById("content2").innerHTML = output;
     $("#content2").fadeIn("slow");
 }
 
+// Takes the specific list-object and its image.
 function showImage(li, value) {
 
+    // Each time an image shows, text regenerate with updated clicks.
+    $("#triesThisGame").html("Tries this game: " + triesThisGame);
+
+    // IF li don't have any child and imageValues contains less than 2 images:
     if (!li.hasChildNodes() && imageValues.length < 2) {
         image = document.createElement("img");
         image.src = value;
         $(image).fadeIn("fast");
         li.appendChild(image);
         image.className += "visible";
-        triesThisGame += 1;
 
         if (imageValues.length == 0) {
+            triesThisGame += 1;
+            triesTotal += 1;
             imageValues.push(value);
             imageIDs.push(li.id);
         } else if (imageValues.length == 1) {
@@ -81,23 +78,29 @@ function showImage(li, value) {
                 imageIDs = [];
 
                 if (clearedImages == imagesArray.length) {
-                    document.getElementById("content2").innerHTML = "<h1 style='text-align: center;'>You did it!</h1>";
-                    $("#content2").fadeOut(1500, function() { memoryGame(); });
+                    $("#content2").fadeOut("slow");
+                    $("#content2").html("<h1 style='text-align: center;'>You did it!</h1>" +
+                                        "<button type='button' onclick(restartGame())>Play again?</button>").fadeIn("slow");
+                    wonGames += 1;
+                    averagePerGame = triesTotal / wonGames;
+                    function restartGame() {
+                        $("#content2").fadeOut(1500, function() { memoryGame(); });
+                    }
                 }
 
             } else {
-                    function hideUnmatchedImages() {
-                        var liOne = document.getElementById(imageIDs[0]);
-                        var liTwo = document.getElementById(imageIDs[1]);
-                        $(liOne).children("img").fadeOut("slow", function() {
-                            $(liOne).children("img").remove();
-                        });
-                        $(liTwo).children("img").fadeOut("slow", function() {
-                            $(liTwo).children("img").remove();
-                        });
-                        imageValues = [];
-                        imageIDs = [];
-                    }
+                function hideUnmatchedImages() {
+                    var liOne = document.getElementById(imageIDs[0]);
+                    var liTwo = document.getElementById(imageIDs[1]);
+                    $(liOne).children("img").fadeOut("slow", function() {
+                        $(liOne).children("img").remove();
+                    });
+                    $(liTwo).children("img").fadeOut("slow", function() {
+                        $(liTwo).children("img").remove();
+                    });
+                    imageValues = [];
+                    imageIDs = [];
+                }
                 setTimeout(hideUnmatchedImages, 1000);
             }
         }

16a64f986472d3a1799cbee848ef74e5a250b1eb
maegz <max.sundberg@hotmail.com>
2016-11-29 13:23:24 +0100

    Mobile friendly fix with background and fading

--- js/jQuery.js
+++ js/jQuery.js
@@ -1,10 +1,11 @@
 // Small jQuery-function to make the footer and jumbotron loop opacity changes slightly.
 $(document).ready(function() {
 
-    $("nav").hide(0).delay(2000).fadeIn(1000);
-    $("footer").hide(0).delay(1000).fadeIn(1000);
+    if ($(window).width() > 480) {
 
-    $(".jumbotron").hide(0).delay(1500).fadeIn(1000).html(
+        $("nav").hide(0).delay(2000).fadeIn(1000);
+        $("footer").hide(0).delay(1000).fadeIn(1000);
+        $(".jumbotron").hide(0).delay(1500).fadeIn(1000).html(
             "<div class='container'>" +
                 "<h1>Max Project</h1>" +
                 "<p>This page was created for the purpouse of testing " +
@@ -13,17 +14,27 @@ $(document).ready(function() {
                 "thing you've ever seen, but instead a rookies hope and " +
                 "desire for something greater to come...</p>" +
             "</div>"
-    );
+        );
 
+        function animateFooterAndJumbotron() {
+            $("footer, #jumbotron").animate({
+                opacity: 0.9}, 3000).animate({
+                opacity: 0.7}, 3000, animateFooterAndJumbotron);
+        }
+        animateFooterAndJumbotron();
 
-
-    function animateFooterAndJumbotron() {
-        $("footer, #jumbotron").animate({
-            opacity: 0.9}, 3000).animate({
-            opacity: 0.7}, 3000, animateFooterAndJumbotron);
+    } else {
+        $(".jumbotron").html(
+            "<div class='container'>" +
+                "<h1>Max Project</h1>" +
+                "<p>This page was created for the purpouse of testing " +
+                "my newly gotten skills in HTML, CSS and JavaScript. " +
+                "So bear in mind that this might not be the most brilliant " +
+                "thing you've ever seen, but instead a rookies hope and " +
+                "desire for something greater to come...</p>" +
+            "</div>"
+        );
     }
-
-    animateFooterAndJumbotron();
 
 });
 
